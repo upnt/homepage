79b9721652362848fdc657b6d5241e9ee4c4dd19
upnt <upnt.github@gmail.com>
2021-04-11 23:28:16 +0900

    delete files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload thank you.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav style={{ background: "#666" }}>
+                    <ul style={{ display: 'flex', listStyle: 'none' }}>
+                        <li style={{ margin: 10 }}><a href="/" style={{ color: "#fff", textDecoration: 'none' }}>Home</a></li>
+                        <li style={{ margin: 10 }}><a href="/about" style={{ color: "#fff", textDecoration: 'none' }}>About</a></li>
+                    </ul>
+                </nav>
+
+                <header style={{ height: 200, background: "#ddd" }}>
+                    header
+                </header>
+
+                <div id="main" style={{ height: 300, background: "#eee" }}>
+                    main
+                </div>
+
+                <footer style={{ height: 100, background: "#666", color: "#fff" }}>
+                    footer
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
