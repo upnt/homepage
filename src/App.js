a2a8d45f38dbe56642c8c2e0df2789748e3e69f4
upnt <upnt.github@gmail.com>
2021-04-15 12:59:31 +0900

    add minutes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
+
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { seconds: 0 };
+        this.state = {
+            seconds: 0.0,
+            minutes: 0
+        };
     }
 
     tick() {
         this.setState(state => ({
-            seconds: state.seconds + 1
+            seconds: state.seconds + 0.1
         }));
+        if(this.state.seconds >= 60) {
+            this.setState(state => ({
+                seconds: 0.0
+            }));
+            this.setState(state => ({
+                minutes: state.minutes + 1
+            }));
+        }
     }
 
     componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
+        this.interval = setInterval(() => this.tick(), 100);
     }
 
     componentWillUnmount() {
@@ -23,9 +35,12 @@ class App extends React.Component {
     render() {
         return (
           <div>
-            Seconds: {this.state.seconds}
+            Seconds: {this.state.seconds.toFixed(1)}
+            <br />
+            Minutes: {this.state.minutes}
           </div>
         );
-      }
+    }
 }
+
 export default App;
